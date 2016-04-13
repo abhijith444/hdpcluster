40830eaa9b88a9db83e61b94abda6ce8cbc64a05
abhijith444 <abhijth8@gmail.com>
2016-04-13 10:59:48 -0500

    Update ambari-install.sh
    Startup mysql. Add databases and users - ambari,hive,oozie,hue.

--- ambari-install.sh
+++ ambari-install.sh
@@ -1,7 +1,24 @@
 wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.0.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
 yum install -y ambari-server
 yum install -y mysql-server mysql-connector-java
+service mysqld start
+
+mysql -e "CREATE DATABASE ambari"
+mysql -e "CREATE DATABASE hive"
+mysql -e "CREATE DATABASE oozie"
+mysql -e "CREATE DATABASE hue"
+
+
 mysql -e "CREATE USER 'ambari'@'%' IDENTIFIED BY 'bigdata';"
+mysql -e "CREATE USER 'ambari'@'localhost' IDENTIFIED BY 'bigdata';"
+mysql -e "CREATE USER 'hive'@'%' IDENTIFIED BY 'bigdata';"
+mysql -e "CREATE USER 'hive'@'localhost' IDENTIFIED BY 'bigdata';"
+mysql -e "CREATE USER 'oozie'@'%' IDENTIFIED BY 'bigdata';"
+mysql -e "CREATE USER 'oozie'@'localhost' IDENTIFIED BY 'bigdata';"
+mysql -e "CREATE USER 'hue'@'%' IDENTIFIED BY 'bigdata';"
+mysql -e "CREATE USER 'hue'@'localhost' IDENTIFIED BY 'bigdata';"
+
 mysql -e "GRANT ALL PRIVILEGES ON ambari.* TO 'ambari';"
 mysql -e "GRANT ALL PRIVILEGES ON hive.* TO 'hive';"
 mysql -e "GRANT ALL PRIVILEGES ON oozie.* TO 'oozie';"
+mysql -e "GRANT ALL PRIVILEGES ON hue.* TO 'hue';"
